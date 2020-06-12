ddc9acb7013f5fdf48f8a0911f41d3000fa6c76b
yq <1173918654@qq.com>
2020-06-12 09:45:48 +0800

    fix

--- RunTime/Shadow/ShadowController.m
+++ RunTime/Shadow/ShadowController.m
@@ -7,15 +7,37 @@
 //
 
 #import "ShadowController.h"
-
+#import "AGView.h"
 @interface ShadowController ()
-
+{
+    BOOL flag;
+    AGView * viewV;
+}
+@property (nonatomic,copy) NSString * isUser;
 @end
 
 @implementation ShadowController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    AGView * viewB = [[AGView alloc] initWithFrame:CGRectMake(0, 100, 100, 100)];
+    viewB.backgroundColor = [UIColor cyanColor];
+    [self.view addSubview:viewB];
+    viewV = viewB;
+//    __block AGView * weakViewB = viewV;
+//    viewB.RBlock = ^(NSInteger tag) {
+//        self->flag = YES;
+//        [weakViewB.btnTitle setTitle:@"哈哈" forState:UIControlStateNormal];
+//    };
     
+//    [UIView animateWithDuration:0.2 animations:^{
+//        self->flag = YES;
+//    }];
+}
+
+- (void)dealloc
+{
+    NSLog(@"我输出了");
 }
 @end
